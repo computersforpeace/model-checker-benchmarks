6ab37c539ac3939d63027753d2b7fd324ffb9192
Brian Norris <banorris@uci.edu>
2012-10-09 11:04:10 -0700

    common benchmarks makefile definitions

---
+++ benchmarks.mk
@@ -0,0 +1,23 @@
+# A few common Makefile items
+
+CC = gcc
+CXX = g++
+
+UNAME = $(shell uname)
+
+LIB_NAME = model
+LIB_SO = lib$(LIB_NAME).so
+
+BASE = ../..
+INCLUDE = -I$(BASE)/include  -I$(BASE)
+CPPFLAGS += $(INCLUDE)
+CFLAGS += $(INCLUDE)
+LDFLAGS += -L$(BASE)/$(LIB_SO) -l$(LIB_NAME)
+
+# Mac OSX options
+ifeq ($(UNAME), Darwin)
+MACFLAGS = -D_XOPEN_SOURCE -DMAC
+CPPFLAGS += $(MACFLAGS)
+CFLAGS += $(MACFLAGS)
+LDFLAGS += $(MACFLAGS)
+endif
