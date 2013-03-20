8c19c4c22a76ba53d7eff91102a489bb44c805d7
Brian Norris <banorris@uci.edu>
2013-03-20 15:57:18 -0700

    bench.sh: don't automatically log to file
    Just print to STDOUT, and the user can manually pipe to a file if they
    want.

--- bench.sh
+++ bench.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
 # A (work-in-progress) test script for running our benchmarks
-# Runs all tests, logging output to a directory named
-# '${BASEDIR}/run-<date-time>', where ${BASEDIR} is either the current
-# directory or the first parameter to this script
+# Runs all tests, with timing information
 
 ## Unfinished benchmarks - do not use
 # queue williams-queue
 
 DATECMD="date +%Y-%m-%d-%R"
 DATE="`${DATECMD}`"
-BASEDIR=.
-
-[ $# -gt 0 ] && [ -d "$1" ] && BASEDIR="$1" && shift
-
-DIR="${BASEDIR}/run-${DATE}"
 
 TESTS="chase-lev-deque/main"
 TESTS+=" spsc-queue/spsc-queue"
@@ -40,14 +33,13 @@ function run_test {
 	shift
 	ARGS="$@"
 	RUN="./run.sh"
-	LOG=${DIR}/log-${COUNT}
 
 	echo "-----------------------------------------------"
 	echo "*******************************"
-	echo "Running test ${COUNT} (${t}): logging to ${LOG}"
+	echo "Running test ${COUNT} (${t})"
 	echo "ARGS=${ARGS}"
 	echo "*******************************"
-	(time ${RUN} ${t} ${ARGS} 2>&1) 2>&1 | tee ${LOG}
+	(time ${RUN} ${t} ${ARGS} 2>&1) 2>&1
 	echo
 	echo "Test done; sleeping for a few seconds"
 	echo
@@ -67,5 +59,9 @@ function run_all_tests {
 	#run_test mpmc-queue/mpmc-queue ${MODEL_ARGS} -- -r 2 -w 2
 }
 
-mkdir ${DIR}
-(cd ..; git log --oneline -1; cd - > /dev/null; git log --oneline -1; echo; run_all_tests) | tee ${DIR}/timing.log
+cd ..
+git log --oneline -1
+cd - > /dev/null
+git log --oneline -1
+echo
+run_all_tests
