77847ecd3fa6a643302770491928787ba143cde1
Brian Norris <banorris@uci.edu>
2013-05-28 15:09:44 -0700

    Makefiles: use appropriate preprocessor vs. compiler flags
    We should differentiate preprocessor vs. C++ compiler vs. C compiler
    flags, according to the CPPFLAGS, CXXFLAGS, and CFLAGS conventions. That
    way we can, for instance, easily add the appropriate --std=XXX flags
    necessary for compiling with a C++11 (or C++0x) or C11 (or C1x)
    compliant compiler.

--- benchmarks.mk
+++ benchmarks.mk
@@ -10,15 +10,24 @@ LIB_SO = lib$(LIB_NAME).so
 
 BASE = ../..
 INCLUDE = -I$(BASE)/include -I../include
-FLAGS := -g
-CPPFLAGS += $(INCLUDE) $(FLAGS)
-CFLAGS += $(INCLUDE) $(FLAGS)
+
+# C preprocessor flags
+CPPFLAGS += $(INCLUDE) -g
+
+# C++ compiler flags
+CXXFLAGS += $(CPPFLAGS)
+
+# C compiler flags
+CFLAGS += $(CPPFLAGS)
+
+# Linker flags
 LDFLAGS += -L$(BASE) -l$(LIB_NAME) -rdynamic
 
 # Mac OSX options
 ifeq ($(UNAME), Darwin)
 MACFLAGS = -D_XOPEN_SOURCE -DMAC
 CPPFLAGS += $(MACFLAGS)
+CXXFLAGS += $(MACFLAGS)
 CFLAGS += $(MACFLAGS)
 LDFLAGS += $(MACFLAGS)
 endif
